424e981938ddc0d5b641449653ce9b812bd53849
yiwai02 <yiwai02@gmail.com>
2022-03-05 16:19:01 -0500

    Added axios and connected dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,26 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 
 import "./Dictionary.css";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("")
 
+    function handleResponse(response){
+        console.log(response.data);
+    }
+
     function search(event){
         event.preventDefault();
-        alert(`Searching for ${keyword}`)
+        alert(`Searching for ${keyword}`);
+
+        //source: https://dictionaryapi.dev/
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function keywordChange(event){
-        setKeyword(event.target.value)
+        setKeyword(event.target.value);
     }
 
     return(
